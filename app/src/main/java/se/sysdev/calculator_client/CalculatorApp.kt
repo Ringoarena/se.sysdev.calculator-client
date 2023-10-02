5efad4afdb2ee5edf21e6d3d2a8e4c448864bb2d
rikard virta <rikard.virta90gmail.com>
2023-10-02 21:14:30 +0200

    base calculation on user input

--- app/src/main/java/se/sysdev/calculator_client/CalculatorApp.kt
+++ app/src/main/java/se/sysdev/calculator_client/CalculatorApp.kt
@@ -3,6 +3,7 @@ package se.sysdev.calculator_client
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import se.sysdev.calculator_client.ui.CalculatorViewModel
@@ -21,39 +23,40 @@ import se.sysdev.calculator_client.ui.CalculatorViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorApp() {
-  Scaffold(topBar = { TopAppBar(title = { Text("Calculator") }) }) {
-    Surface(
-      modifier = Modifier
-        .fillMaxSize()
-        .padding(it)
-    ) {
-      val calculatorViewModel: CalculatorViewModel = viewModel()
-      val calculatorUiState by calculatorViewModel.uiState.collectAsState()
-      Column {
-        Text(text = "${calculatorUiState.calculatedValue}")
-        OutlinedTextField(value = "asd", onValueChange = InputHandler()::handleValueChanged )
-        Button(onClick = {
-          calculatorViewModel.calculateAddition(2,3)
-//          CalculatorServiceFacade().calculateAddition(2, 1)
-        }) {
-          Text(text = "Calculate")
+    Scaffold(topBar = { TopAppBar(title = { Text("Calculator") }) }) { it ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            val calculatorViewModel: CalculatorViewModel = viewModel()
+            val calculatorUiState by calculatorViewModel.uiState.collectAsState()
+            Column {
+                calculatorUiState.calculatedValue?.let {
+                  Text(text = "$it")
+                }
+                OutlinedTextField(
+                    value = calculatorViewModel.operandA,
+                    onValueChange = { calculatorViewModel.updateOperandA(it) },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.NumberPassword)
+                )
+                OutlinedTextField(
+                    value = calculatorViewModel.operandB,
+                    onValueChange = { calculatorViewModel.updateOperandB(it) },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.NumberPassword)
+                )
+                Button(
+                    onClick = { calculatorViewModel.calculateAddition() }
+                ) {
+                    Text(text = "Calculate")
+                }
+            }
         }
-      }
     }
-  }
-}
-class InputHandler {
-  fun handleValueChanged(input: String) {
-    println(input)
-  }
-}
-
-fun handleValueChange(input: String) {
-  println(input)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CalculatorAppPreview() {
-  CalculatorApp()
+    CalculatorApp()
 }
--- app/src/main/java/se/sysdev/calculator_client/ui/CalculatorViewModel.kt
+++ app/src/main/java/se/sysdev/calculator_client/ui/CalculatorViewModel.kt
@@ -1,16 +1,33 @@
 package se.sysdev.calculator_client.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class CalculatorViewModel : ViewModel() {
-  private val _uiState = MutableStateFlow(CalculatorUiState())
-  val uiState: StateFlow<CalculatorUiState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(CalculatorUiState())
+    val uiState: StateFlow<CalculatorUiState> = _uiState.asStateFlow()
+    var operandA by mutableStateOf("")
+        private set
+    var operandB by mutableStateOf("")
+        private set
 
-  fun calculateAddition(a: Int, b: Int) {
-    val sum = a + b
-    _uiState.value = CalculatorUiState(sum)
-  }
+    fun updateOperandA(newValue: String) {
+        operandA = newValue
+    }
+
+    fun updateOperandB(newValue: String) {
+        operandB = newValue
+    }
+
+    fun calculateAddition() {
+        val aNum = operandA.toInt()
+        val bNum = operandB.toInt()
+        val sum = aNum + bNum
+        _uiState.value = CalculatorUiState(sum)
+    }
 }
